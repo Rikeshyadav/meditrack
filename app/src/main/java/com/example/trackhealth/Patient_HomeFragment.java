5ad431bd16e83d5fcd47ff488c53457aaa2dd13f
Jugalcody <jugalnerist@gmail.com>
2024-03-08 20:05:11 +0530

    conected patient home page to server

--- app/src/main/java/com/example/trackhealth/Patient_HomeFragment.java
+++ app/src/main/java/com/example/trackhealth/Patient_HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.trackhealth;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Rect;
 import android.os.Bundle;
 
@@ -15,62 +17,188 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Patient_HomeFragment extends Fragment {
     RecyclerView recyclerView;
     Toolbar toolbar;
+    ImageView refresh;
+    ProgressBar progressBar;
+    TextView nodata;
+    LottieAnimationView empty;
+    patient_homeAdapter adapter;
+    List arr=new ArrayList<>();
     DrawerLayout drawerLayout;
-    List<datamodel>data;
-    public Patient_HomeFragment() {
-        // Required empty public constructor
-    }
-
+    SharedPreferences sp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_patient__home, container, false);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(null);
+
         recyclerView=view.findViewById(R.id.patient_recyclview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
-        data=new ArrayList<>();//initialized
-        datamodel ob1=new datamodel(R.drawable.doctorimage_2,"DR. rikesh","heart","trims");
-        data.add(ob1);
-        datamodel ob2=new datamodel(R.drawable.img,"DR. Jugal","eye","nerist");
-        data.add(ob2);
-        datamodel ob3=new datamodel(R.drawable.logo,"DR. min","nose","Aiims");
-        data.add(ob3);
-        datamodel ob4=new datamodel(R.drawable.img_1,"DR. fun","nose","Aiims");
-        data.add(ob4);
-        datamodel ob5=new datamodel(R.drawable.img_1,"DR. richo","nose","Aiims");
-        data.add(ob5);
-        // datamodel ob4=new datamodel();
-        //   ob4.setDp(R.drawable.img);    not able to take due to the way we define in datamodel
-        int spacingInPixels= 2;
-        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+        sp= requireActivity().getSharedPreferences("user", Context.MODE_PRIVATE);
+
+
+        progressBar=view.findViewById(R.id.patientpage_home_progress);
+        progressBar.setVisibility(View.VISIBLE);
+        empty=view.findViewById(R.id.home_patientpage_lottie);
+        nodata=view.findViewById(R.id.home_patientpage_nodata);
+        refresh=view.findViewById(R.id.refresh_home_patientpage);
+        refresh.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
-                outRect.left=spacingInPixels*6;
-                outRect.right=spacingInPixels*6;
-                outRect.top=spacingInPixels;
-                outRect.bottom=spacingInPixels;
+            public void onClick(View view) {
+                searchDoctors(sp.getString("phone",""));
             }
         });
-        recyclerView.setAdapter(new patient_homeAdapter(data));
         toolbar = view.findViewById(R.id.customtool);
-        //((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
-       // ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-       // ((AppCompatActivity) requireActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.back); // Set your hamburger icon
-
+        ((AppCompatActivity) getActivity()).setSupportActionBar(null);
         drawerLayout = requireActivity().findViewById(R.id.drawer_layout1);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(requireActivity(), drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
          toggle.syncState();
-
+        searchDoctors(sp.getString("phone",""));
         return view;
     }
+
+
+    public void searchDoctors(String ph){
+        String temp = "https://demo-uw46.onrender.com/api/patient/getDetails";
+        try {
+            progressBar.setVisibility(View.VISIBLE);
+            refresh.setVisibility(View.GONE);
+            empty.setVisibility(View.GONE);
+            nodata.setVisibility(View.GONE);
+            HashMap<String, String> jsonobj = new HashMap<>();
+            jsonobj.put("phone", ph);
+            JsonObjectRequest j = new JsonObjectRequest(Request.Method.POST, temp, new JSONObject(jsonobj), new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    //pb.setVisibility(View.GONE);
+                    try {
+
+                        if (Boolean.parseBoolean(response.getString("success"))) {
+                            JSONArray ja = response.getJSONArray("doctoradd");
+                            if (ja.length()>0) {
+                                arr = filterArray(response.getJSONArray("doctoradd"));
+                                if (arr.size() > 0) {
+                                    adapter = new patient_homeAdapter(arr);
+                                    recyclerView.setAdapter(adapter);
+                                    progressBar.setVisibility(View.GONE);
+                                    refresh.setVisibility(View.GONE);
+                                    empty.setVisibility(View.GONE);
+                                    nodata.setVisibility(View.GONE);
+                                    recyclerView.setHasFixedSize(true);
+                                } else {
+                                    progressBar.setVisibility(View.GONE);
+                                    empty.setVisibility(View.VISIBLE);
+                                    refresh.setVisibility(View.GONE);
+                                    nodata.setVisibility(View.VISIBLE);
+                                }
+                            }else{
+                                progressBar.setVisibility(View.GONE);
+                                empty.setVisibility(View.VISIBLE);
+                                refresh.setVisibility(View.GONE);
+                                nodata.setVisibility(View.VISIBLE);
+                            }
+                        }
+                        else{
+                            progressBar.setVisibility(View.GONE);
+                            empty.setVisibility(View.GONE);
+                            nodata.setVisibility(View.GONE);
+                            refresh.setVisibility(View.VISIBLE);
+                            Toast.makeText(getActivity(),"failed to load",Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException e) {
+                        progressBar.setVisibility(View.GONE);
+                        empty.setVisibility(View.GONE);
+                        nodata.setVisibility(View.GONE);
+                        refresh.setVisibility(View.VISIBLE);
+                        Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressBar.setVisibility(View.GONE);
+                    empty.setVisibility(View.GONE);
+                    nodata.setVisibility(View.GONE);
+                    refresh.setVisibility(View.VISIBLE);
+                    Toast.makeText(getActivity(),"check internet connectivity", Toast.LENGTH_SHORT).show();
+                }
+            });
+            RequestQueue q = Volley.newRequestQueue(getActivity());
+            RetryPolicy retryPolicy = new DefaultRetryPolicy(
+                    30000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            );
+            j.setRetryPolicy(retryPolicy);
+            q.add(j);
+
+
+        } catch (
+                Exception e) {
+            //   pb.setVisibility(View.GONE);
+            //       pb.setVisibility(View.GONE);
+
+        }
+
+    }
+
+
+
+    public List filterArray(JSONArray jsonArray) throws JSONException {
+        List<List> outer=new ArrayList<>();
+
+        for(int i=0;i<jsonArray.length();i++){
+
+            JSONObject j=jsonArray.getJSONObject(i);
+            List<String> inner=new ArrayList<>();
+            if(j.getString("pending").equals("false")){
+                inner.add("");
+                inner.add(j.getString("doctor_name"));
+                inner.add(j.getString("specification"));
+                try {
+                    inner.add(j.getString("clinic_name"));
+                }catch(Exception e){
+                    inner.add(j.getString("doctor_name"));
+                }
+                inner.add(j.getString("qualification"));
+                outer.add(inner);
+
+            }
+
+        }
+
+        return outer;
+    }
+
+
 }
--- app/src/main/java/com/example/trackhealth/patient_homeAdapter.java
+++ app/src/main/java/com/example/trackhealth/patient_homeAdapter.java
@@ -1,6 +1,5 @@
 package com.example.trackhealth;
 
-import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,34 +10,33 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class patient_homeAdapter extends RecyclerView.Adapter<patient_homeAdapter.myholder> {
 
-    private static List<datamodel>data;
+    private static List<List>data;
 
-    public patient_homeAdapter(List<datamodel> data) {
+    public patient_homeAdapter(List<List> data) {
         this.data = data;
     }
 
     @NonNull
     @Override
-    public myholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public patient_homeAdapter.myholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pateint_iui,parent,false);
         return new myholder(view);
 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull myholder holder, int position) {
-        datamodel currentItem=data.get(position);
-        holder.dp.setImageResource(data.get(position).getDp());
-        holder.txt1.setText(data.get(position).getText1());
-        holder.txt2.setText(data.get(position).getText2());
-        holder.txt3.setText(data.get(position).getText3());
-
+    public void onBindViewHolder(@NonNull patient_homeAdapter.myholder holder, int position) {
+        //holder.dp.setImageResource(data.get(position));
+        holder.dname.setText("Dr. "+data.get(position).get(1).toString());
+        holder.dspec.setText(data.get(position).get(2).toString());
+        holder.dhospital.setText(data.get(position).get(3).toString());
+        holder.dqua.setText(data.get(position).get(4).toString());
     }
+
 
     @Override
     public int getItemCount() {
@@ -47,13 +45,14 @@ public class patient_homeAdapter extends RecyclerView.Adapter<patient_homeAdapte
 
     public static class myholder extends RecyclerView.ViewHolder implements View.OnClickListener{
         ImageView dp;
-        TextView txt1,txt2,txt3;
+        TextView dname, dspec, dhospital,dqua;
         public myholder(@NonNull View itemView) {
             super(itemView);
             dp=itemView.findViewById(R.id.dp_patient_page_doctor);
-            txt1=itemView.findViewById(R.id.itemtext);
-            txt2=itemView.findViewById(R.id.itemtext2);
-            txt3=itemView.findViewById(R.id.itemtext3);
+            dname =itemView.findViewById(R.id.dname_patient_homepage);
+            dspec =itemView.findViewById(R.id.dspec_patient_homepage);
+            dhospital =itemView.findViewById(R.id.dhospital_patient_homepage);
+            dqua=itemView.findViewById(R.id.dqua_patient_homepage);
             itemView.setOnClickListener(this);
         }
 
@@ -61,7 +60,6 @@ public class patient_homeAdapter extends RecyclerView.Adapter<patient_homeAdapte
         public void onClick(View v) {
             int position=getAdapterPosition();
             if(position !=RecyclerView.NO_POSITION){
-                datamodel clickedItem=data.get(position);
                 Intent intent=new Intent(v.getContext(), user_report_homepage.class);
                 v.getContext().startActivity(intent);
             }
